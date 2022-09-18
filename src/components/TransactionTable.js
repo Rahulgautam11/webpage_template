3718ad2f71c6b0e98ac86c8d81339d1e7b8390fa
RAHUL GAUTAM <94911559+Rahulgautam11@users.noreply.github.com>
2022-09-18 23:42:08 +0530

    Update TransactionTable.js

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,27 +1,86 @@
-const TransactionTable = (props) => {
+const TransactionTable = () => {
   return (
-    <div className="table-container">
-      <h3 className="table-title">Active Deposits</h3>
-      <span className="transaction-no">2</span>
-      <button type="button" className="add-btn">
-        +
-      </button>
+    <>
+      <div className="table-container">
+        <h3 className="table-title">Active Deposits</h3>
+        <p className="transaction-no">2</p>
+        <button type="button" className="add-btn">
+          +
+        </button>
 
-      <table>
-        <thead>
-          <tr>
-            <th>participents</th>
-            <th>MOVE IN DATE</th>
-            <th>RENT</th>
-            <th>DEPOSIT</th>
-            <th>
-              STATUS
-              <span className="status-color"></span>
-            </th>
-          </tr>
-        </thead>
-      </table>
-    </div>
+        <table width="80%">
+          <thead>
+            <tr>
+              <th>participents</th>
+              <th>MOVE IN DATE</th>
+              <th>RENT</th>
+              <th>DEPOSIT</th>
+              <th>STATUS</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>771 Lost Round</td>
+              <td>23 February 2020</td>
+              <td>$3000</td>
+              <td>$9000</td>
+              <td>Awating Bank Processing</td>
+            </tr>
+            <tr>
+              <td>771 Lost Round</td>
+              <td>23 February 2020</td>
+              <td>$3000</td>
+              <td>$9000</td>
+              <td>Awating Bank Processing</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div className="active-btn">
+          <button type="button">All Active Deposite</button>
+        </div>
+      </div>
+
+      <div className="table-container">
+        <h3 className="table-title">Active Deposits</h3>
+        <p className="transaction-no">2</p>
+        <button type="button" className="add-btn">
+          +
+        </button>
+
+        <table width="80%">
+          <thead>
+            <tr>
+              <th>participents</th>
+              <th>MOVE IN DATE</th>
+              <th>RENT</th>
+              <th>DEPOSIT</th>
+              <th>STATUS</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>771 Lost Round</td>
+              <td>23 February 2020</td>
+              <td>$3000</td>
+              <td>$9000</td>
+              <td>Awating Bank Processing</td>
+            </tr>
+            <tr>
+              <td>771 Lost Round</td>
+              <td>23 February 2020</td>
+              <td>$3000</td>
+              <td>$9000</td>
+              <td>Awating Bank Processing</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div className="active-btn">
+          <button type="button">Closed Deposite</button>
+        </div>
+      </div>
+    </>
   );
 };
 export default TransactionTable;
